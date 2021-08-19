e6a1e12b732b3d94772c6358fa5b2d424e2fcf7e
Marcelo Gomes <celogomesmartins@gmail.com>
2021-08-19 15:14:45 -0300

    Update - Flipbook is working

--- packages/flip-book/src/FlipBook.js
+++ packages/flip-book/src/FlipBook.js
@@ -4,26 +4,23 @@ import { getFlipbook } from './utils'
 import { Iframe } from './components'
 import S from './styles'
 
-const FlipBook = ({ pdfUrl, modifiedAt }) => {
-  console.log('pdfurl', pdfUrl)
-  const file = window.btoa(pdfUrl)
+const FlipBook = (props) => {
+  const { pdfurl, modifiedAt, width = '100%', height = '800px' } = props
+  const file = window.btoa(pdfurl)
   const query = qs.stringify({ file })
-  const flipbookUrl = `http://localhost:3000/public/pdf-flipbook/index.html?${query}`
+  const flipbookUrl = `http://localhost:30214/index.html?${query}`
 
   const handleOnRetry = (something) => {
     console.log('something', something)
   }
 
   return (
-    <S.Wrapper>
-      <h3>Hey Jeyhun</h3>
-      <Iframe
-        isLoading={false}
-        hasError={false}
-        src={flipbookUrl}
-        onRetry={handleOnRetry}
-      />
-    </S.Wrapper>
+    <Iframe
+      isLoading={false}
+      hasError={false}
+      src={flipbookUrl}
+      onRetry={handleOnRetry}
+    />
   )
 }
 
